0155ed1f91873344ab2ca475dfb27ea8ab615b6e
luis.carvajal <alfilsoftbusiness@gmail.com>
2023-02-23 01:09:02 +0000

    python exercises class 1

---
+++ python/example-1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+#this is al comment
+print("hello guys 1")
+print("hello guys 2")
+print("hello guys 3")
+print('''
+
+test -->print("hello guys 2")
+
+''')
+
+text="hello fucking world\n"
+text2="fuck you!!"
+print(text+" "+text2)
---
+++ python/exercise-1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+print("Hello welcome to the coffee shop\n")
+
+
+menu="coffee, tea and wather"
+name=input("Hello what's your name\n")
+beverage=input("What do you want to drink "+ name +" from the menu "+menu+"\n")
+
+print("your "+beverage+" gonna be ready soon \n")
