021659569ec3c14ab906bfe6ebc8521f6a879f28
EC2 Default User <ec2-user@ip-172-31-42-137.ec2.internal>
2023-02-24 00:00:30 +0000

    class 2 python

--- python/exercise-1.py
+++ python/exercise-1.py
@@ -8,3 +8,13 @@ name=input("Hello what's your name\n")
 beverage=input("What do you want to drink "+ name +" from the menu "+menu+"\n")
 
 print("your "+beverage+" gonna be ready soon \n")
+
+price=4
+units=int(input("how many units you want?  \n"))
+
+max_units=100
+if units>max_units:
+    print("we just have "+str(max_units)+" unitsgit sta")
+else:
+    print("the value of your order is: "+str(price*units)+ " USD")
+    print(name+ " your "+ beverage + " is going to be ready soon")
