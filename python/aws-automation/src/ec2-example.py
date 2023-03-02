bec9a416b2f455ba637d85b522109397c10d1bbc
luis.carvajal <alfilsoftbusiness@gmail.com>
2023-03-02 23:55:00 +0000

    class 5

---
+++ python/aws-automation/src/ec2-example.py
@@ -0,0 +1,13 @@
+import boto3
+
+
+AWS_REGION="us-east-1"
+EC2_RESOURCE=boto3.resource("ec2",region_name=AWS_REGION)
+
+instances = EC2_RESOURCE.instances.all()
+
+for instance in instances:
+    print(f"EC2 id: {instance.id} state: {instance.state['Name']}")
+
+
+
